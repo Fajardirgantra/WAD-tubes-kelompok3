e034d59cadf14bf40fab7b80ab5e9a4306ce45a0
memorism <rezaabdllh9@gmail.com>
2023-12-27 17:54:00 +0700

    nambah route

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,16 +5,30 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('home') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    @if (Auth::user()->usertype == 'admin')
+                        {{-- <x-nav-link :href="route('user_index')" :active="request()->routeIs('user_index')">
+                            {{ __('Users') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('ruangan_index')" :active="request()->routeIs('ruangan_index')">
+                            {{ __('Ruangan') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('asset_index')" :active="request()->routeIs('asset_index')">
+                            {{ __('Asset') }}
+                        </x-nav-link> --}}
+                        <x-nav-link :href="route('pemeliharaan_index')" :active="request()->routeIs('pemeliharaan_index')">
+                            {{ __('Pemeliharaan') }}
+                        </x-nav-link>
+                    @endif
                 </div>
             </div>
 
@@ -67,7 +81,7 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+            <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
         </div>
