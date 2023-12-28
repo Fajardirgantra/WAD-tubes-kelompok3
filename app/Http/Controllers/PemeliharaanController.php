4612b6837b3d5933f1f8c3cf01b9ef1034373851
memorism <rezaabdllh9@gmail.com>
2023-12-28 14:48:43 +0700

    Controller

---
+++ app/Http/Controllers/PemeliharaanController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Pemeliharaan;
+use App\Models\Asset;
+
+class PemeliharaanController extends Controller
+{
+    public function index()
+    {
+        $pemeliharaans = Pemeliharaan::all();
+        return view('admin.pemeliharaan_index', compact('pemeliharaans'));
+    }
+    public function create()
+    {
+        //
+    }
+    public function store(Request $request)
+    {
+        //
+    }
+    public function edit($pemeliharaan_id)
+    {
+        //
+    }
+    public function update(Request $request, $pemeliharaan_id)
+    {
+        //
+    }
+    public function destroy($pemeliharaan_id)
+    {
+        //
+    }
+}
