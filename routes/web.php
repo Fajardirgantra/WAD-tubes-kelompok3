c3282ea8cac547c39ef7319260caf26cedba4429
noviarachma2002 <116255185+noviarachma2002@users.noreply.github.com>
2023-12-28 11:41:59 +0700

    progres routes index

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/complain/index', [ComplainController::class, 'index'])->name('complain_index');
