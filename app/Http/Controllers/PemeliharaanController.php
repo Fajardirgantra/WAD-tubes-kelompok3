2ccd8572fbf202137fc4ba7ce499c49d9302b6c0
memorism <rezaabdllh9@gmail.com>
2023-12-29 06:33:31 +0700

    update edit

--- app/Http/Controllers/PemeliharaanController.php
+++ app/Http/Controllers/PemeliharaanController.php
@@ -15,16 +15,27 @@ class PemeliharaanController extends Controller
     }
     public function create()
     {
-        $pemeliharaans = Pemeliharaan::all();
-        return view('admin.pemeliharaan_create', compact('pemeliharaans'));
+        $assets = Asset::all();
+        return view('admin.pemeliharaan_create', compact('assets'));    
     }
     public function store(Request $request)
     {
-        //
+        $foto = fake() -> uuid() . '.' . $request->file('foto')->extension();
+        $request->file('foto')->move(public_path('/pemeliharaan/bukti'), $foto);
+        Pemeliharaan::create([
+            'asset_id' => $request -> asset_id,
+            'jenis_perbaikan' => $request -> jenis_perbaikan,
+            'kegiatan' => $request -> kegiatan,
+            'foto' => "/pemeliharaan/bukti/$foto",
+            'tanggal_pemeliharaan' => $request -> tanggal_pemeliharaan
+        ]);
+        return redirect('/pemeliharaan/index');
     }
     public function edit($pemeliharaan_id)
     {
-        //
+        $pemeliharaan = Pemeliharaan::find($pemeliharaan_id);
+        $assets = Asset::all();
+        return view('admin.pemeliharaan_edit', compact('pemeliharaan', 'assets')); 
     }
     public function update(Request $request, $pemeliharaan_id)
     {
